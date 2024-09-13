5f81dc4c2c71db77c4a570cced7ccac565e8e0d7
Sanyam Soni <105146300+Sanyamsoni77@users.noreply.github.com>
2024-09-13 22:06:23 +0530

    up date

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -18,7 +18,28 @@ const LandingPage = () => {
             <div className="lname">SONI</div>
             <div className="additional-text">Android Developer</div>
             <div className="additional-text-sec">Tech | Content Creator | Android | Web </div>
-            <h2>Education</h2>
+            
+            <h1 className="header-title">ABOUT ME</h1>
+            <p className="cursive-text">
+              I am a passionate Android developer with a strong background in IT operations. I thrive on challenges and enjoy finding innovative solutions to complex problems. With a deep love for technology and a constant desire to learn and grow new tech concepts and also share same knowledge by creating content and posting on social platforms, I am always up-to-date with the latest industry trends and updates, and improving all the mix of tech trends day by day.
+            </p>
+            <h2>Android Developer</h2>
+            <p className="cursive-text">
+              I have a wealth of experience in developing high-quality Android applications that deliver exceptional user experiences. From concept to deployment, I am skilled in the entire app development lifecycle, including designing intuitive user interfaces, implementing robust functionality, and ensuring smooth performance across various devices and screen sizes. My expertise also extends to integrating APIs, optimizing app performance, and incorporating cutting-edge technologies to create innovative mobile solutions.
+            </p>
+            <h2>Tech Enthusiast & Digital Creator</h2>
+            <p className="cursive-text">
+              A passionate digital creator with a deep love for all things tech. On my platform, I focus on delivering a blend of the latest tech trends, updates, and insights. Whether it's exploring emerging technologies, diving into cutting-edge gadgets, or breaking down complex tech concepts, my goal is to make tech knowledge accessible to everyone.
+            </p>
+            <p className="cursive-text">
+              From gadget spec updates to product launches, I cover it all—keeping you informed about the latest advancements in the tech world.
+            </p>
+            <p className="cursive-text">
+              In addition to content creation, I've been working on exciting android projects.
+              Currently working on Project Detectify, an offline mobile app with features like real-time barcode scanning, facial analysis, and object detection. And there's much more to come! Stay tuned as I continue to innovate, explore, and bring new ideas to life in the tech space.
+            </p>
+
+            <h2 className="projectSS">EDUCATION</h2>
             <ul>
               <li>
                 <strong>Bachelor's Degree in Computer Applications (BCA)</strong>, Govt. College, Dharamshala Affiliated HPU, Shimla.
